8892a859d72ed51fa97e274b89c19638d67f8e75
Peter Ahinko <ahinko@users.noreply.github.com>
2015-09-21 14:49:42 +0200

    Fixing tests

--- tests/Phapi/Middleware/Serializer/Jsonp/JsonpTest.php
+++ tests/Phapi/Middleware/Serializer/Jsonp/JsonpTest.php
@@ -163,4 +163,32 @@ class JsonpTest extends TestCase {
         $this->setExpectedException('\RuntimeException', 'Serializer could not retrieve unserialized body');
         $serializer($request, $response, $next);
     }
+    
+        public function testNoContentType()
+    {
+        // Serializer
+        $serializer = \Mockery::mock('Phapi\Serializer\Serializer[serialize]', [['application/json', 'text/json']]);
+        $serializer->shouldReceive('serialize')->with(['username' => 'phapi'])->andReturn("{ 'username': 'phapi' }");
+
+        // Container
+        $container = \Mockery::mock('Phapi\Contract\Di\Container');
+        $container->shouldReceive('offsetGet')->with('acceptTypes')->andReturn([]);
+        $container->shouldReceive('offsetSet')->with('acceptTypes', ['application/json', 'text/json']);
+        $serializer->setContainer($container);
+
+        $serializer->registerMimeTypes();
+
+        // Request
+        $request = \Mockery::mock('Psr\Http\Message\ServerRequestInterface');
+
+        $response = \Mockery::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('hasHeader')->with('Content-Type')->andReturn(false);
+        //$response->shouldReceive('getHeaderLine')->with('Content-Type')->andReturn('application/json');
+        //$response->shouldReceive('getUnserializedBody')->andReturn([ 'username' => 'phapi' ]);
+        //$response->shouldReceive('withBody')->with(\Mockery::type('Psr\Http\Message\StreamInterface'))->andReturnSelf();
+
+        $serializer($request, $response, function ($request, $response) {
+            return $response;
+        });
+    }
 }
